838e684fdc9651d1d93f325998ec28ed379a52d4
choiminseok <catower917@gmail.com>
2023-08-16 09:56:45 +0900

    file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def HelloWorld():
     return 'Hello, World~~!'
+
+if __name__=='__main__':
+    app.run('0.0.0.0',port=80,threaded=True)
