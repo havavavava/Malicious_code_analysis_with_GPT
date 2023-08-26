cb69e34e72e49b2aa715268f7ce3079ca787dc1b
choiminseok <catower917@gmail.com>
2023-08-27 01:24:00 +0900

    index(/), home(/home), upload(/upload) 3개 페이지 생성

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, url_for, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def HelloWorld():
-    return 'Hello, World~~!'
+def main():
+    return render_template('index.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__=='__main__':
     app.run('0.0.0.0',port=80,threaded=True)
