6279c1235076734a5852304312c9feb911a65642
choiminseok <catower917@gmail.com>
2023-09-07 01:48:06 +0900

    csv file upload

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request, session, url_for, redirect
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/upload')
+
+@app.route('/upload',methods=['GET','POST'])
 def upload():
-    return render_template('upload.html')
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save("./uploads/" + secure_filename(f.filename))
+        return render_template('/home.html')
+    else:
+        return render_template('/upload.html')
 
 @app.route('/home')
 def home():
