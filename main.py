cd4c385482607397d815061ab53c21c942ebf7dc
choiminseok <catower917@gmail.com>
2023-09-07 04:02:17 +0900

    추가해야될 기능

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, session, url_for, redirect
 from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+
+#엑셀 -> csv 변환
+#def excel_to_csv():
+
+#csv파일 분석 후 PE헤더 정보 출력 및 그래프로 나타내기
+#def print_csv():
 
 @app.route('/')
 def main():
@@ -18,6 +25,9 @@ def upload():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
+
 
 if __name__=='__main__':
     app.run('0.0.0.0',port=80,threaded=True)
